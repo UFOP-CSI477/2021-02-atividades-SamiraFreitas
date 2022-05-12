5a8fd3f16311df8e179fdd3e36290c8d89517e40
SamiraFreitas <samfreitascomp@gmail.com>
2022-05-12 16:50:17 -0300

    Update index.js

--- Atividades/atividade-pratica-01/ex_01/index.js
+++ Atividades/atividade-pratica-01/ex_01/index.js
@@ -1,24 +1,3 @@
-function enviarDados(){
-   
-    const val = document.getElementById("valor").value;
-    const date = document.getElementById("data").value;
-    const ch = document.getElementById("chave").value;
-    const vch = document.getElementById("vchave").value;
-    const bc = document.getElementById("bancos").value;
-    
-    const table = document.getElementById("table");
-    const row = document.createElement("row");
-    row.innerHTML=  `<tr>
-    <td>${val}</td>
-    <td>${date}</td>
-    <td>${ch}</td>
-    <td>${vch}</td>
-    <td>${bc}</td>
-    `
-    table.appendChild(row);
-    
-}
-
 
 function cadDados (valor, data, chave, valordachave, banco){
     var tb = document.getElementById("table");
