16c097be8ba042d41c965c2f34282b3c24918d06
SamiraFreitas <73719899+SamiraFreitas@users.noreply.github.com>
2022-05-26 20:09:07 -0300

    Update api.js

--- Atividades/atividade-pratica-01/ex_02/api.js
+++ Atividades/atividade-pratica-01/ex_02/api.js
@@ -1,6 +1,31 @@
+function preencherSelectMds(data){   
+    let de = document.getElementById('converter-de');
+
+    for (let index in data){
+        const code = index
+        const name = data[index]
+        
+        let option = document.createElement("option");
+        option.value = code;
+        option.innerHTML = `${name}`;
+        de.appendChild(option);
+    }
+    let para = document.getElementById('converter-para');
+    for (let index in data){
+        const code = index
+        const name = data[index]
+        
+        let option = document.createElement("option");
+        option.value = code;
+        option.innerHTML = `${name}`;
+        para.appendChild(option);
+    }
+}
+
+
 function Moedas(){
     fetch('https://economia.awesomeapi.com.br/json/available/uniq')
         .then(response => response.json())
-        .then(data => preencherSelectMoedas(data))
+        .then(data => preencherSelectMds(data))
         .catch(error => console.error(error.message))
 }
