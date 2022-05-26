82a531dcac0890f9966672fbb08c2c9e5c8e662e
SamiraFreitas <73719899+SamiraFreitas@users.noreply.github.com>
2022-05-26 20:09:45 -0300

    Update index.js

---
+++ Atividades/atividade-pratica-01/ex_02/index.js
@@ -0,0 +1,11 @@
+function pegaValor(){
+    let val = document.getElementById("valor").value
+    valor = parseFloat(valor)
+    val = parseFloat(val)
+    alert('passei)
+    
+  let cv = document.getElementById("cv")
+  cv.innerHTML = ''
+  cv.innerHTML = valor
+  
+    }
